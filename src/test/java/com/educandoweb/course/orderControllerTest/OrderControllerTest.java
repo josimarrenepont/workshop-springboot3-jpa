03b10147eb41dae3d1cdab908f762dfe48bb19a4
josimarrenepont <jnrenepont@hotmail.com>
2024-12-17 22:45:51 -0300

    testCreate | OrderControllerTest

--- src/test/java/com/educandoweb/course/orderControllerTest/OrderControllerTest.java
+++ src/test/java/com/educandoweb/course/orderControllerTest/OrderControllerTest.java
@@ -3,7 +3,6 @@ package com.educandoweb.course.orderControllerTest;
 import com.educandoweb.course.controller.OrderController;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.OrderItem;
-import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.dto.OrderDto;
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -18,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,6 +33,7 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderControllerTest {
@@ -54,14 +56,14 @@ public class OrderControllerTest {
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         mockMvc = MockMvcBuilders.standaloneSetup(orderController)
-                .setControllerAdvice() // Adicione controllers globais se necessário
+                .setControllerAdvice()
                 .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
                 .build();
 
         user = new User(1L, "user", "user@email.com", "1234567", "1234567");
         order = new Order(1L, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user);
-
     }
+
     @Test
     void testFindAll() throws Exception {
         List<OrderDto> orderDtoList = Collections.singletonList(new OrderDto(order));
@@ -83,6 +85,23 @@ public class OrderControllerTest {
                 .contentType(MediaType.APPLICATION_JSON));
 
         result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(order.getId()))
+                .andExpect(jsonPath("$.moment").value(order.getMoment().toString()))
+                .andExpect(jsonPath("$.orderStatus").value(order.getOrderStatus().toString()));
+    }
+    @Test
+    void testCreate() throws Exception{
+
+        when(orderService.create(any(Order.class))).thenReturn(order);
+
+        String orderJson = "{\"moment\": \"2019-06-20T19:53:07Z\", \"orderStatus\": \"PAID\", \"client\": {\"id\": 1}}";
+
+        ResultActions result = mockMvc.perform(post("/orders")
+                        .content(orderJson)
+            .contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(order.getId()))
                 .andExpect(jsonPath("$.moment").value(order.getMoment().toString()))

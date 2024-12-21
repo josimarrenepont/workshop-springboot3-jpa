03a85a083ab335013b3bf71d50af6c21060ef49c
josimarrenepont <jnrenepont@hotmail.com>
2024-12-21 00:36:21 -0300

    testFindAll | OrderServiceTest

---
+++ src/test/java/com/educandoweb/course/orderServiceTest/OrderServiceTest.java
@@ -0,0 +1,67 @@
+package com.educandoweb.course.orderServiceTest;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.services.OrderService;
+import com.educandoweb.course.services.PaymentService;
+import com.educandoweb.course.services.StockService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    @Mock
+    private OrderRepository repository;
+    @Mock
+    private StockService stockService;
+    @Mock
+    private PaymentService paymentService;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    private Order order;
+    private User user;
+    private Product product;
+    private OrderItem orderItem;
+
+    @BeforeEach
+    void SetUp(){
+        user = new User(1L, "user", "user@email.com", "1234567", "1234567");
+        order = new Order(1L, Instant.parse("2019-06-20T15:20:01Z"), OrderStatus.PAID, user);
+        product = new Product(1L, "Cell Phone", "Iphone 15 pro",
+                1500.0, "imgUrl", 7);
+        orderItem = new OrderItem(null, product, 2, product.getPrice());
+        order.setItems(Collections.singleton(orderItem));
+    }
+    @Test
+    void testFindAll(){
+        when(repository.findAll()).thenReturn(Collections.singletonList(order));
+
+        List<Order> result = orderService.findAll();
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(order.getId(), result.get(0).getId());
+        verify(repository, times(1)).findAll();
+    }
+}
